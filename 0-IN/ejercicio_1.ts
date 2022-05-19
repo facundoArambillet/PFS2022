510e460388c1418005086b5936107b011c3fb58e
Facundo Arambillet <Facundoarambillet@gmail.com>
2022-05-19 19:54:25 -0300

    .

---
+++ 0-IN/ejercicio_1.ts
@@ -0,0 +1,48 @@
+// Camino hacia el supermercado
+console.log('Paso 1: Hacer lista de productos ');
+console.log('Paso 2: Movilizarce hacia el supermercado ');
+console.log('Paso 3: Entrar');
+console.log('Paso 4: Agarrar carrito de compras ');
+console.log('Paso 5: Buscar productos ');
+console.log('Paso 6: Ir hacia la caja');
+console.log('Paso 7: Bajar los productos del carrito');
+console.log('Paso 8: Pagar ');
+console.log('Paso 9: Salir del supermercado');
+console.log('Paso 10: Volver a casa');
+
+// Hacer una torta
+/*
+console.log('Paso 1: Comprar ingredientes');
+console.log('Paso 2: Prender el horno ');
+console.log('Paso 3: Batir yemas de huevos en un recipiente');
+console.log('Paso 4: Agregar azucar  ');
+console.log('Paso 5: Aregar Leche ');
+console.log('Paso 6: Agregar harina');
+console.log('Paso 7: Batir claras de huevo en otro recipiente');
+console.log('Paso 8: Mezclar ambos recipientes ');
+console.log('Paso 9: Meter en el horno');
+console.log('Paso 10: Esperar 60 minutos' );
+console.log('Paso 11: Sacar del horno');
+*/
+
+// Crear un posteo en Facebook
+
+/*
+console.log('Paso 1: Abrir facebook');
+console.log('Paso 2: cliquear "que estoy pensando"');
+console.log('Paso 3: Escribir posteo');
+console.log('Paso 4: Revisar posteo ');
+console.log('Paso 5: Apretar boton "Publicar" ');
+*/
+
+// Salir a correr
+/*
+console.log('Paso 1: Levantarse de la cama');
+console.log('Paso 2: Vestirse ');
+console.log('Paso 3: Desayunar');
+console.log('Paso 4: Lavarse los dientes');
+console.log('Paso 5: Agarrar llaves ');
+console.log('Paso 6: Salir de casa');
+console.log('Paso 7: Caminar hacia el lugar donde correremos');
+console.log('Paso 8: Empezar a correr');
+*/
