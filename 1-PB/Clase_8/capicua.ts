7d42e92145cf28dcf509e93fe72c94cb4720e420
Facundo Arambillet <Facundoarambillet@gmail.com>
2022-06-09 19:00:47 -0300

    ..

--- 1-PB/Clase_8/capicua.ts
+++ 1-PB/Clase_8/capicua.ts
@@ -1,7 +1,7 @@
 import * as readlineSync from 'readline-sync';
 let palabraUsuario : string = readlineSync.question('Ingrese una palabra: ');
+let booleano : boolean;
 function determinarCapicua(palabra : string){
-    let booleano : boolean;
     for(let i = 0; i< palabra.length /2; i++) {
         if(palabra[i] == palabra[palabra.length - i - 1]){
             booleano = true
